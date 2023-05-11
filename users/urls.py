d662ca1419070cdb7d4f56f498f7ab006b064240
WooJin_Chung <wjin9707@hufs.ac.kr>
2023-05-11 10:13:00 +0900

    Feat: register validation

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
     path('changeInfo/', views.ChageInfo, name='changeinfo'),
+
+    path('api/v1/id-validation', views.IdValidation.as_view(), name='id_validation'), 
+    path('api/v1/hp-validation', views.HPValidation.as_view(), name='hp_validation'), 
+    path('api/v1/email-validation', views.EmailValidation.as_view(), name='email_validation'), 
 ]
